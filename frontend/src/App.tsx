451fc97b90c919f389df7de94377dbafadfabafb
karishmapatil0000 <karishmapatil0000@gmail.com>
2026-03-12 18:48:08 +0530

    Initial React project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Smart HRMS</h1>
+    </div>
+  );
+}
+
+export default App;
